831bd686320b8c067bbed2fdc633f10f10a5a16e
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-03-28 11:52:40 -0300

    feat: authentication with jwt

--- api/src/app/auth/auth.controller.ts
+++ api/src/app/auth/auth.controller.ts
@@ -1,8 +1,24 @@
-import { Controller } from '@nestjs/common';
-
-import { AuthService } from './auth.service';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor() {}
+
+  @Get('public')
+  getPublicResouce() {
+    return {
+      message: 'Este recurso é público e não requer autenticação.',
+    };
+  }
+
+  @Get('protected')
+  @UseGuards(AuthGuard('jwt'))
+  getProtectedResouce(@Req() { user }: Request) {
+    return {
+      message: 'Este recurso é protegido e requer autenticação.',
+      user: user,
+    };
+  }
 }
--- api/src/app/auth/stategies/jwt-strategy.ts
+++ api/src/app/auth/stategies/jwt-strategy.ts
@@ -1,4 +1,40 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { passportJwtSecret } from 'jwks-rsa';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+interface IJWT {
+  sub: string;
+  'cognito:groups': string[];
+  iss: string;
+  version: number;
+  client_id: string;
+  origin_jti: string;
+  token_use: string;
+  scope: string;
+  auth_time: number;
+  exp: number;
+  iat: number;
+  jti: string;
+  username: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://cognito-idp.us-east-1.amazonaws.com/${process.env.SSO_USER_POOL_ID}/.well-known/jwks.json`,
+      }),
+    });
+  }
+
+  async validate(payload: IJWT): Promise<{ userId: string }> {
+    return { userId: payload?.sub };
+  }
+}
