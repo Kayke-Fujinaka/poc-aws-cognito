a72f54537e03d215c7fa2483744cc4510866a61b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-03-29 10:10:20 -0300

    feat: add documentation each route

--- api/src/app/app.controller.ts
+++ api/src/app/app.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get()
+  @ApiOperation({ summary: 'Verifica a saúde da aplicação.' })
+  @ApiResponse({ status: 200, description: 'Aplicação rodando.' })
   health() {
     return {
       status: 200,
-      message: 'Running Application',
+      message: 'Aplicação rodando.',
     };
   }
 }
--- api/src/app/auth/controllers/auth.controller.ts
+++ api/src/app/auth/controllers/auth.controller.ts
@@ -1,21 +1,40 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { Roles } from '../decorators/roles.decorator';
 import { Role } from '../interfaces';
 import { AuthService } from '../services/auth.services';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('public')
+  @ApiOperation({ summary: 'Acessa recurso público.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Este recurso é público e não requer autenticação.',
+  })
   getPublicResource() {
     return this.authService.public();
   }
 
   @Get('protected')
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Acessa recurso protegido.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Este recurso é protegido e requer autenticação.',
+  })
+  @ApiResponse({ status: 401, description: 'Não autorizado.' })
   getProtectedResource() {
     return this.authService.protected();
   }
@@ -23,6 +42,15 @@ export class AuthController {
   @Get('admin')
   @UseGuards(AuthGuard('jwt'))
   @Roles(Role.Admin)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Acessa recurso de administração.' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Este recurso é protegido, requer autenticação e permissão de administrador.',
+  })
+  @ApiResponse({ status: 401, description: 'Não autorizado.' })
+  @ApiResponse({ status: 403, description: 'Acesso negado.' })
   getAdminResource() {
     return this.authService.admin();
   }
